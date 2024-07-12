9a23b4d04374cbbf1878303d120ecfa63fecf9f6
Joot111 <joaoamoroso04@gmail.com>
2024-07-12 20:33:44 +0100

    ok

--- Project_ArqueoList/Project_ArqueoList/Startup.cs
+++ Project_ArqueoList/Project_ArqueoList/Startup.cs
@@ -3,58 +3,56 @@ using Project_ArqueoList.Data;
 
 namespace Project_ArqueoList
 {
-    namespace Projeto_ArqueoList
+    public class Startup
     {
-        public class Startup
+        public Startup(IConfiguration configuration)
         {
-            public Startup(IConfiguration configuration)
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // Configuração do banco de dados
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
             {
-                Configuration = configuration;
+                app.UseDeveloperExceptionPage();
             }
-
-            public IConfiguration Configuration { get; }
-
-            // This method gets called by the runtime. Use this method to add services to the container.
-            public void ConfigureServices(IServiceCollection services)
+            else
             {
-                services.AddControllersWithViews();
-
-                // Configuração do banco de dados
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlServer(
-                        Configuration.GetConnectionString("DefaultConnection")));
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-                }
-                else
-                {
-                    app.UseExceptionHandler("/Home/Error");
-                    app.UseHsts();
-                }
-                app.UseHttpsRedirection();
-                app.UseStaticFiles();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
 
-                app.UseRouting();
-
-                app.UseAuthentication();
-                app.UseAuthorization();
-
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllerRoute(
-                        name: "default",
-                        pattern: "{controller=Home}/{action=Index}/{id?}");
-                    endpoints.MapRazorPages();
-                });
-
-                // Configure logging
-                logger.LogInformation("Application started.");
-            }
+            // Configure logging
+            logger.LogInformation("Application started.");
         }
     }
+}
