9cf9f1113e4d66cb91fccaa60824e4fc507db975
Joot111 <joaoamoroso04@gmail.com>
2024-07-12 20:01:05 +0100

    Add project files.

---
+++ Project_ArqueoList/Project_ArqueoList/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_ArqueoList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
