4d205218050eaa12f011ad857f45afc59aaf9b61
TiagoDias2 <tiagomiguel082004@gmail.com>
2024-07-13 20:01:52 +0100

    Página Inicial

--- Project_ArqueoList/Project_ArqueoList/Controllers/HomeController.cs
+++ Project_ArqueoList/Project_ArqueoList/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Project_ArqueoList.Data;
 using Project_ArqueoList.Models;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Project_ArqueoList.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // Carregar artigos validados do banco de dados
+            var artigos = await _context.Artigos
+                .Where(a => a.validado)
+                .OrderByDescending(a => a.data_validacao)
+                .ToListAsync();
+
+            return View(artigos);
         }
 
         public IActionResult Privacy()
