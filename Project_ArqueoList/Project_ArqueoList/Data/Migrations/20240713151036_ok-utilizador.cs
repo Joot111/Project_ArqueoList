377b52b73773498247b9873c00456ee11f7f3664
Joot111 <joaoamoroso04@gmail.com>
2024-07-13 18:22:51 +0100

    ok utilizador(inicial)

---
+++ Project_ArqueoList/Project_ArqueoList/Data/Migrations/20240713151036_ok-utilizador.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_ArqueoList.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class okutilizador : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Utilizador",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Utilizador",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Imagem",
+                table: "Artigos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Utilizador");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Utilizador");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Imagem",
+                table: "Artigos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
--- Project_ArqueoList/Project_ArqueoList/Models/UtilizadorViewModel.cs
+++ Project_ArqueoList/Project_ArqueoList/Models/UtilizadorViewModel.cs
@@ -1,40 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Project_ArqueoList.Models
 {
-    public class Utilizador
+    public class UtilizadorViewModel
     {
-        public Utilizador()
-        {
-            ListaArtigo = new HashSet<Artigo>();
-        }
-
-        [Key]
-        public int idUtilizador { get; set; }
-
         [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
         [StringLength(30, ErrorMessage = "Atingiu o limite de characters.")]
         public string Username { get; set; }
 
-        //[Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
+        // [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
         //[StringLength(20, ErrorMessage = "Atingiu o limite de characters.")]
-        //[BindNever]
         //public string Password { get; set; }
 
-        //[BindNever]
-        //public string Role { get; set; }
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
+        public string Role { get; set; }
 
         [DataType(DataType.Date)] // informa a View de como deve tratar este atributo
         [DisplayFormat(ApplyFormatInEditMode = true,
                DataFormatString = "{0:dd-MM-yyyy}")]
         [Required(ErrorMessage = "A {0} é de preenchimento obrigatório")]
         public DateTime Data_Nascimento { get; set; }
-
-        [StringLength(40)]
-        [BindNever]
-        public string UserId { get; set; }
-
-        public ICollection<Artigo> ListaArtigo { get; set; }
     }
 }
